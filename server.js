f8c224215829d590094e3c4e179e5c9c7a637869
SarcasticJynx <jrapsins@purdue.edu>
2018-12-06 14:44:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-//var mysql = require('mysql');
+var mysql = require('mysql');
 var port = process.env.port || 1337;
 
 //var connection = mysql.createConnection({
