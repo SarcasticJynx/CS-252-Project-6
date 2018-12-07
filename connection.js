cb86af23dec9394a1228e8bf83636c2f8ae1f099
jpark1997 <43394859+jpark1997@users.noreply.github.com>
2018-12-07 11:15:24 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
 });
 connection.connect(function(err) {
   var sql = "INSERT INTO budgeteering (ID, PW, item) VALUES ('abc', '1234', 'itemA')";
-  con.query(sql, function (err, result) {
+  connection.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
