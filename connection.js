28527e5b427b85811b5d29d50e838e3c3f5358cc
jpark1997 <43394859+jpark1997@users.noreply.github.com>
2018-12-07 01:58:42 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
-//var app = express();
+var app = express();
 
 var connection = mysql.createConnection({
     host: 'mydb.ics.purdue.edu',
@@ -8,3 +8,15 @@ var connection = mysql.createConnection({
     database: 'park862'
 });
 connection.connect();
+
+app.post('park862', function(req, res, next){
+   var cope = req.body.params;
+   var query = connection.query('insert into cope set ?', cope, function(err, result) {
+     if (err) {
+       console.error(err);
+       return res.send(err);
+     } else {
+       return res.send('Ok');
+     }
+});
+app.listen(port);
